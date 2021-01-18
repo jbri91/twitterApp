8028cfefadab8955e260ee3068dc63ce7cb873c6
Johnny Briones <jbrrii91@gmail.com>
2021-01-18 18:33:25 -0500

    added routes to app.py

--- app.py
+++ app.py
@@ -29,6 +29,10 @@ nasa = requests.get('https://api.twitter.com/1.1/search/tweets.json', params=pay
 def index():
     return app.send_static_file('index.html')
 
+@app.route('/random')
+@app.route('/search')
+def home_page():
+
 @app.route('/', methods=['GET'])
 def hello():
     return jsonify({'Response': 'This is my Twitter Showcase App'})
