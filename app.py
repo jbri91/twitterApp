20faefad3597d8966cd90d1d0ac058dead97efb2
Johnny Briones <jbrrii91@gmail.com>
2021-01-18 16:12:40 -0500

    app.py

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ import requests
 import json
 from config import token_secret
 import os
-from boto.s3.connection import S3Connection
-
-s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
 
 
 
