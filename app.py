ef85ae52e6dced503818b44b7a26da5c426f841a
Johnny Briones <jbrrii91@gmail.com>
2021-04-09 21:24:49 -0400

    increase count

--- app.py
+++ app.py
@@ -51,9 +51,9 @@ class RandomTweet(Resource):
     def get(self, user):
         payload6 = {'q': 'from:' + user, 
         'result_type': 'recent',
-        'count': 5}
+        'count': 20}
         results = requests.get('https://api.twitter.com/1.1/search/tweets.json', params=payload6, headers=headers).json()
-        number = randint(1, 5)
+        number = randint(1, 20)
         return jsonify(results['statuses'][number])
 
 api.add_resource(RandomTweet, '/api/random-tweet/<string:user>')
